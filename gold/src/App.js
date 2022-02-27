784661954b46b20c1b6a25a9df9c0bb13148911e
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2022-02-27 00:06:39 -0600

    added link to streaming music services

--- gold/src/App.js
+++ gold/src/App.js
@@ -11,6 +11,12 @@ function App() {
   return (
     <main className="bg-white bg-blend-soft-light container mx-auto my-auto mt-16">
       <Player />
+      <div className="mt-16 mb-16 text-center">
+        <a href="https://smarturl.it/66dfqb">
+          <p className="pt-16 font-mono text-xl">Music
+          </p>
+        </a>
+      </div>
       <Footer />
     </main>
   );
